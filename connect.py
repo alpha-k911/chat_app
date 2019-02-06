2530d25dab30380041ae1e57494ace0d50b97b94
HarshVaragiya <harshvaragiya8@gmail.com>
2019-02-07 01:24:14 +0530

    Hot it should be done

--- connect.py
+++
@@ -1,6 +0,0 @@
-import socket
-import sys
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = int(sys.argv[1])
-s.connect(('127.0.0.1',port))
-s.send("hi".encode())
--- file.py
+++
@@ -1,7 +0,0 @@
-creating a node  => s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-conneting to a domain => host_ip = socket.gethostbyname("name of the host :google.com")
-
-port=80
-
-connecting to server s.connect((host_ip, port))
